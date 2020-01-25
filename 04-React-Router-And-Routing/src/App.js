dc945ff179d3dcd5a0bd4d0e8e7f0167071e2d6d
Aditya Hajare <aditya43@gmail.com>
2020-01-25 17:59:10 +0530

    React router dom

--- 04-React-Router-And-Routing/src/App.js
+++ 04-React-Router-And-Routing/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
-const TopicsPage = () => (
+const TopicsPage = props => (
     <div>
+        <button onClick={() => props.history.push('/topics/12')}><h2>Topic 12</h2></button>
+        <br />
+        <Link to='/topics/13'><h2>Topic 13</h2></Link>
         <h1>TOPICS PAGE</h1>
     </div>
 );
 
-const TopicDetail = () => (
+const TopicDetail = (props) => (
     <div>
         <h1>TOPIC DETAIL PAGE</h1>
+        <h2>Topic Id: {props.match.params.topicId}</h2>
     </div>
 );
 
