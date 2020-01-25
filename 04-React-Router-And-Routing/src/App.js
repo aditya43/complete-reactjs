9b44abef679194824d7e3ce107337f265676ce26
Aditya Hajare <aditya43@gmail.com>
2020-01-25 18:12:56 +0530

    Update menu-items to navigate withRouter() HOC

--- 04-React-Router-And-Routing/src/App.js
+++ 04-React-Router-And-Routing/src/App.js
@@ -1,33 +1,15 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const TopicsPage = props => (
-    <div>
-        <button onClick={() => props.history.push('/topics/12')}><h2>Topic 12</h2></button>
-        <br />
-        <Link to='/topics/13'><h2>Topic 13</h2></Link>
-        <h1>TOPICS PAGE</h1>
-    </div>
-);
-
-const TopicDetail = (props) => (
-    <div>
-        <h1>TOPIC DETAIL PAGE</h1>
-        <h2>Topic Id: {props.match.params.topicId}</h2>
-    </div>
-);
 
 function App () {
     return (
         <div>
             <Switch>
                 <Route exact path='/' component={HomePage} />
-                <Route exact path='/topics' component={TopicsPage} />
-                <Route path='/topics/:topicId' component={TopicDetail} />
             </Switch>
         </div>
     );
