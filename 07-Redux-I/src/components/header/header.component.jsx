6d535e5a232e3939733d2be05913c513b4a016f3
Aditya Hajare <aditya43@gmail.com>
2020-01-30 08:10:37 +0530

    Cart icon, cart dropdown component, cart reducer

--- 07-Redux-I/src/components/header/header.component.jsx
+++ 07-Redux-I/src/components/header/header.component.jsx
@@ -8,7 +8,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.componenet';
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link to="/" className='logo-container'>
             <Logo className='logo' />
@@ -24,12 +24,15 @@ const Header = ({ currentUser }) => (
             }
             <CartIcon />
         </div>
-        <CartDropdown />
+        {
+            hidden ? null : <CartDropdown />
+        }
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
